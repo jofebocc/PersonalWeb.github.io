f15c10477176d7bd04ea66849d2ceba07b1c9dd9
jofebocc <jofebocc@gmail.com>
2023-07-01 01:21:55 -0500

    some fixes

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Project(models.Model):
     title = models.CharField(max_length=264)
-    description = models.TextField(max_length=264)
+    description = models.TextField(max_length=1000)
     image = models.ImageField(max_length=264,upload_to="projects")
     information = models.URLField(blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
